7bea5a1982c359db9d49b47d9eada3cb7fb6b72c
Luke Nguyen <lukenagooyen@gmail.com>
2020-10-09 17:48:05 -0400

    changes to procfile

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser=require('body-parser')
 const {check, validationResult} = require('express-validator')
 const expressLayouts = require('express-ejs-layouts')
 const app = express()
-const port = 8000
+const port = process.env.port || 8000
 
 
 // Static Files
